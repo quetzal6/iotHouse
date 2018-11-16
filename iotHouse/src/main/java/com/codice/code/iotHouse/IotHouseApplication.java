75663502aa60423215acadb29ff905303df37e47
Quetzal Hernandez <quetzal@MacBook-Air-de-Quetzal.local>
2018-11-15 18:48:53 -0600

    first commint

---
+++ iotHouse/src/main/java/com/codice/code/iotHouse/IotHouseApplication.java
@@ -0,0 +1,12 @@
+package com.codice.code.iotHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotHouseApplication.class, args);
+	}
+}
